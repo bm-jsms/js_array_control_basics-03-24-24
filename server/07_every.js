bb444f0c57f67b6e80c6971b6791ccd7b8769089
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-03-25 17:18:13 -0500

    every - exercise

--- server/07_every.js
+++ server/07_every.js
@@ -3,3 +3,26 @@ const numbers = [1, 44, 66, 57, 82, 87, 23];
 const rta = numbers.every((i) => i <= 50);
 
 console.log(rta);
+
+const team = [
+	{
+		name: 'Nicolas',
+		age: 12,
+	},
+	{
+		name: 'Andrea',
+		age: 8,
+	},
+	{
+		name: 'Zulema',
+		age: 17,
+	},
+	{
+		name: 'Santiago',
+		age: 18,
+	},
+];
+
+const rta2 = team.every((i) => i.age >= 15);
+
+console.log(rta2);
