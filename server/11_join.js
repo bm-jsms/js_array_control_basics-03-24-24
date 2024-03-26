a2a6c9b6531f9a9a1d1f5c5ec34c025f4818e17e
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-03-26 16:31:34 -0500

    join - split

---
+++ server/11_join.js
@@ -0,0 +1,11 @@
+const array = ['Hola', 'Mundo', 1, 2, 3, 4];
+
+const rst = array.join(' - ');
+
+console.log(rst);
+console.log(typeof rst);
+
+const rst2 = rst.split(' - ');
+
+console.log(rst2);
+console.log(typeof rst2);
